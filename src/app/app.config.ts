4c95f7fb7db4dfef51887afaec52eb43c2c1de81
André Luferat <contato@luferat.net>
2025-09-26 20:48:57 -0300

    Front-end acessível

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "ngcrud-93a6f", appId: "1:1043187476281:web:777f337c87a8d171d22ae8", storageBucket: "ngcrud-93a6f.firebasestorage.app", apiKey: "AIzaSyDHQPWxYc2Xc2xXwW2dh6bg1Bup7vjYNrk", authDomain: "ngcrud-93a6f.firebaseapp.com", messagingSenderId: "1043187476281" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+// src/app/app.route.ts
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Contacts } from './contacts/contacts';
+import { About } from './about/about';
+
+// Nome do site para compor o `title`
+const siteName = "NgLegal";
+
+export const routes: Routes = [
+    // Rotas vazias redirecionam para a `/home` ← Sempre a primeira
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+    // Páginas { rota, componente, título }
+    { path: 'home', component: Home, title: siteName },
+    { path: 'contacts', component: Contacts, title: `${siteName} - Faça Contato` },
+    { path: 'about', component: About, title: `${siteName} - Sobre` },
+
+    // Rota coringa para redirecionar caminhos inválidos ← Sempre a última
+    { path: '**', redirectTo: '/home' }
+];
